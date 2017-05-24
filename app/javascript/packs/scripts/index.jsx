dd2129114b005782164fad3544be94726bc227c7
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-05-23 21:03:26 -0300

    Initial commit

---
+++ app/javascript/packs/scripts/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class EasyRank extends Component {
+
+  render() {
+    return (
+      <h1>Hello felipinho</h1>
+    )
+  }
+}
+
+export default EasyRank;
