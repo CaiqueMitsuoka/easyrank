e1a5d98a3434df4e03a5bb8059addf9c96d86277
Felipe Soares <felipeluizsoares@gmail.com>
2017-05-30 23:40:43 -0300

    Update Home component to render the Teams component

--- app/javascript/packs/scripts/containers/Home.jsx
+++ app/javascript/packs/scripts/containers/Home.jsx
@@ -2,18 +2,16 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import Teams from '../components/Teams'
 
 class Home extends Component {
   render() {
     return (
       <div>
         <h2>Easy Rank Teams</h2>
-
+        <Teams/>
         <div>
-          <Link to='/teams'> Times </Link>
           <Link to='/team/new'> Criar um time </Link>
-          <Link to='/team/delete'> Deletar um time </Link>
-          <Link to='/team/edit'> Editar um time </Link>
         </div>
       </div>
     )
