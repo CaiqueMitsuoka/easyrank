e5fea7e9b8efc1e4b2790a546339a4bc60b83a8f
Felipe Soares <felipeluizsoares@gmail.com>
2017-05-30 23:42:44 -0300

    Update Edit component to render the Form component

--- app/javascript/packs/scripts/containers/team/Edit.jsx
+++ app/javascript/packs/scripts/containers/team/Edit.jsx
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import Form from '../../components/Form'
 
 class Edit extends Component {
+  constructor (props) {
+    super(props)
+  }
+
   render() {
+    console.log(this.props.match.params);
+
     return (
       <div>
-        <h2>Edit</h2>
-
+        <h2>Edit Team</h2>
+        <Form />
         <Link to='/'> Voltar </Link>
       </div>
     )
