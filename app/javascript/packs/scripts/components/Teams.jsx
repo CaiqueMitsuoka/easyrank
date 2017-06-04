bc21f2112e866b00d22ce62cd1279f8e5961df62
Felipe Soares <felipeluizsoares@gmail.com>
2017-06-04 00:31:20 -0300

    Render teams at Teams component

--- app/javascript/packs/scripts/components/Teams.jsx
+++ app/javascript/packs/scripts/components/Teams.jsx
@@ -3,53 +3,46 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-class Teams extends Component {
-  constructor() {
-    super()
-
-    this.state =  { teams: [{ teamId:1, position: 1, name: 'São Paulo', points: 32 }] }
-
-    this.handleDelete = this.handleDelete.bind(this)
-  }
-
-  handleDelete () {
+const Teams = (props) => {
+  const handleDelete = () => {
     if (confirm('You really want to delete this team?')) {
       return alert('The team has been deleted')
     }
   }
 
-  render() {
-    const { teams } = this.state
-    return (
-      <div>
-        <h2>Teams</h2>
+  return (
+    <div>
+      <h2>Teams</h2>
 
-        <table>
-          <tbody>
-            <tr>
-              <th> Position </th>
-              <th> Team </th>
-              <th> Points </th>
-              <th> Edit </th>
-              <th> Delete </th>
+      <table>
+        <tbody>
+          <tr>
+            <th> Position </th>
+            <th> Team </th>
+            <th> Points </th>
+            <th> Edit </th>
+            <th> Delete </th>
+          </tr>
+
+          {
+            props.teams.map((team, index) =>
+            <tr key={index}>
+              <td> {team.position} </td>
+              <td> {team.name} </td>
+              <td> {team.points} </td>
+              <td> <Link to={`/team/${team.teamId}/edit`}> &#x270E; </Link> </td>
+              <td> <button onClick={this.handleDelete}>&#x2716;</button> </td>
             </tr>
+          )
+        }
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-            {
-              teams.map((team, index) =>
-              <tr key={index}>
-                <td> {team.position} </td>
-                <td> {team.name} </td>
-                <td> {team.points} </td>
-                <td> <Link to={`/team/${team.teamId}/edit`}> &#x270E; </Link> </td>
-                <td> <button onClick={this.handleDelete}>&#x2716;</button> </td>
-              </tr>
-            )
-          }
-          </tbody>
-        </table>
-      </div>
-    )
-  }
+Teams.propTypes = {
+  teams: PropTypes.array.isRequired
 }
 
 export default Teams
--- app/javascript/packs/scripts/containers/Home.jsx
+++ app/javascript/packs/scripts/containers/Home.jsx
@@ -3,18 +3,27 @@ import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import Teams from '../components/Teams'
-import { fetchAllTeams } from '../serviceAPI'
+import { fetchAllTeams, teams } from '../serviceAPI'
 
 class Home extends Component {
-  componentDidMount () {
-    fetchAllTeams()
+  constructor () {
+    super()
+
+    this.state = { teams : [] }
   }
 
-  render() {
+  componentDidMount () {
+    fetchAllTeams()
+      .then((teams) => this.setState({ teams }))
+  }
+
+  render () {
+    const { teams } = this.state
+
     return (
       <div>
         <h2>Easy Rank Teams</h2>
-        <Teams/>
+        <Teams teams={teams}/>
         <div>
           <Link to='/team/new'> Criar um time </Link>
         </div>
